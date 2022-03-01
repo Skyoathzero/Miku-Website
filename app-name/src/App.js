4ad5df56a7e4af11416cef768d126c17b78d7ac1
Skyoathzero <73295100+Skyoathzero@users.noreply.github.com>
2022-03-01 16:37:44 +0700

    finished clock , working on date and animation next

--- app-name/src/App.js
+++ app-name/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Redirect, Routes  } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Redirect, Routes } from 'react-router-dom'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 // PAGES / CONTENT
 import Main from './pages/Main.js'
@@ -15,15 +17,15 @@ function App() {
                             <img src={require('./image/cloud.jpg')} />
                         </div>
                     </div>
-                    <div className="bottom-bg"/>
+                    <div className="bottom-bg" />
                     {/* CONTENT FOR ROUTER */}
                     <Routes>
-                        <Route path="/" element={<Main/>} />
+                        <Route path="/" element={<Main />} />
                     </Routes>
                     <div className="footer">
                         <p className="myname">hello</p>
                         <p className="date">hello</p>
-                        <p className="time">hello</p>
+                        <Time />
                     </div>
                 </section>
             </section>
@@ -31,4 +33,49 @@ function App() {
     )
 }
 
+function Time() {
+    const [hours,setHours]     = useState(new Date().getHours())
+    const [minutes,setMinutes] = useState(new Date().getMinutes())
+    const [seconds,setSeconds] = useState(new Date().getSeconds())
+    
+    useEffect(()=>{
+        setInterval(()=>{
+            let tHours = new Date().getHours()
+            if(tHours.toString().length < 2){
+                setHours('0'+tHours.toString())
+            }
+            else{setHours(tHours.toString())}
+            let tMinutes = new Date().getMinutes()
+            if(tMinutes.toString().length < 2){
+                setMinutes('0'+tMinutes.toString())
+            }
+            else{setMinutes(tMinutes.toString())}
+            let tSeconds = new Date().getSeconds()
+            if(tSeconds.toString().length < 2){
+                setSeconds('0'+tSeconds.toString())
+            }
+            else{setSeconds(tSeconds.toString())}
+        },1000)
+    })
+    return (
+        <div className="time">
+            <section>
+                <p>{hours + 'H'}</p>
+            </section>
+            <span>:</span>
+            <section>
+                <p>{minutes + 'M'}</p>
+            </section>
+            <span>:</span>
+            <section>
+                <p>{seconds + 'S'}</p>
+            </section>
+        </div>
+    )
+}
+
 export default App
+
+
+
+
