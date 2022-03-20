11b70907dc4aea8e7413ff16bb2d9595997aa7fa
Skyoathzero <73295100+Skyoathzero@users.noreply.github.com>
2022-03-20 16:19:01 +0700

    partially done with about me

--- app-name/src/App.js
+++ app-name/src/App.js
@@ -2,8 +2,9 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Redirect, Routes } from 'react-router-dom'
 import { useState } from 'react'
 import { useEffect } from 'react'
-import {BsFillMoonFill,BsFillSunFill} from 'react-icons/bs'
-import {AiFillHeart} from 'react-icons/ai'
+import { BsFillMoonFill, BsFillSunFill } from 'react-icons/bs'
+import { AiFillHeart } from 'react-icons/ai'
+import background from './image/cloud-anime.gif'
 
 // PAGES / CONTENT
 import Main from './pages/Main.js'
@@ -23,11 +24,13 @@ function App() {
                     </div>
                     <div className="bottom-bg" />
                     {/* CONTENT FOR ROUTER */}
+                    <About />
                     <Routes>
                         <Route path="/" element={<Main />} />
                     </Routes>
+                    
                     <div className="footer">
-                        <Dates/>
+                        <Dates />
                         <Time />
                     </div>
                 </section>
@@ -37,34 +40,34 @@ function App() {
 }
 
 function Time() {
-    const [hours,setHours]     = useState(new Date().getHours())
-    const [minutes,setMinutes] = useState(new Date().getMinutes())
-    const [seconds,setSeconds] = useState(new Date().getSeconds())
-    const [isDay,setIsDay]     = useState(false)
-    useEffect(()=>{
-        setInterval(()=>{
+    const [hours, setHours] = useState(new Date().getHours())
+    const [minutes, setMinutes] = useState(new Date().getMinutes())
+    const [seconds, setSeconds] = useState(new Date().getSeconds())
+    const [isDay, setIsDay] = useState(false)
+    useEffect(() => {
+        setInterval(() => {
             let tHours = new Date().getHours()
-            if(tHours.toString().length < 2){
-                setHours('0'+tHours.toString())
+            if (tHours.toString().length < 2) {
+                setHours('0' + tHours.toString())
             }
-            else{setHours(tHours.toString())}
+            else { setHours(tHours.toString()) }
             let tMinutes = new Date().getMinutes()
-            if(tMinutes.toString().length < 2){
-                setMinutes('0'+tMinutes.toString())
+            if (tMinutes.toString().length < 2) {
+                setMinutes('0' + tMinutes.toString())
             }
-            else{setMinutes(tMinutes.toString())}
+            else { setMinutes(tMinutes.toString()) }
             let tSeconds = new Date().getSeconds()
-            if(tSeconds.toString().length < 2){
-                setSeconds('0'+tSeconds.toString())
+            if (tSeconds.toString().length < 2) {
+                setSeconds('0' + tSeconds.toString())
             }
-            else{setSeconds(tSeconds.toString())}
-            if(tHours > 4 && tHours<16){
+            else { setSeconds(tSeconds.toString()) }
+            if (tHours > 4 && tHours < 16) {
                 setIsDay(true)
             }
-            else{
+            else {
                 setIsDay(false)
             }
-        },1000)
+        }, 1000)
     })
     return (
         <div className="time">
@@ -80,7 +83,7 @@ function Time() {
                 <p>{seconds + 'S'}</p>
             </section>
             <span className='day-night-icon'>
-                {isDay?(<BsFillSunFill/>):(<BsFillMoonFill/>)}
+                {isDay ? (<BsFillSunFill />) : (<BsFillMoonFill />)}
             </span>
         </div>
     )
@@ -88,38 +91,53 @@ function Time() {
 
 function Dates() {
     // ARRAY
-    const aMonth =['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const aMonth = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     const aDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     // ARRAY
 
-    const[day,setDay]     = useState([new Date().getDate() , new Date().getDay()])
-    const[month,setMonth] = useState(new Date().getMonth())
-    const[year,setYear]   = useState(new Date().getFullYear())
+    const [day, setDay] = useState([new Date().getDate(), new Date().getDay()])
+    const [month, setMonth] = useState(new Date().getMonth())
+    const [year, setYear] = useState(new Date().getFullYear())
 
-    useEffect(()=>{
-        setInterval(()=>{
-            setDay([new Date().getDate() , new Date().getDay()])
+    useEffect(() => {
+        setInterval(() => {
+            setDay([new Date().getDate(), new Date().getDay()])
             setMonth(new Date().getMonth())
             setYear(new Date().getFullYear())
 
-        },100000)
+        }, 100000)
     })
     return (
         <div className='date'>
             <section className='day-dates'>
                 <p>
-                    {aDay[day[1]] + " "+day[0].toString() + ", "}
+                    {aDay[day[1]] + " " + day[0].toString() + ", "}
                 </p>
             </section>
             <section className='month-year'>
                 <p>
-                    {aMonth[month] + " " + year} 
+                    {aMonth[month] + " " + year}
                 </p>
             </section>
         </div>
     )
 }
 
+
+
+function About() {
+    return (
+        <>
+            <div className='about-cnt' style={{backgroundImage:`url(${background})`}}>
+                <section id='main'>
+                    <section id='twitter'></section>
+                    <section id='github'></section>
+                </section>
+            </div>
+        </>
+
+    )
+}
 
 export default App
 
