0ad5002c74e42c0826b0f1048a7668c5d588081c
Skyoathzero <73295100+Skyoathzero@users.noreply.github.com>
2022-08-23 17:18:31 +0700

    Revised the music player yet again.

--- app-name/src/MusicPlayer.js
+++ app-name/src/MusicPlayer.js
@@ -9,8 +9,10 @@ import $, { data } from 'jquery';
 import Slider from 'rc-slider';
 import { BsFillVolumeUpFill, BsArrowsAngleExpand, BsFillCaretDownFill } from 'react-icons/bs'
 import { SiApplemusic } from 'react-icons/si'
-
+import { useSwiper } from 'swiper/react';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectFade } from 'swiper';
+import 'swiper/css/effect-fade';
 import 'rc-slider/assets/index.css';
 const formatTime = require('format-duration')
 
@@ -23,9 +25,12 @@ function MusicPlayer() {
     const [duration, setDuration] = useState(0);
     const [volume, setVolume] = useState(0.5);
     const [progessObj, setProgressObj] = useState({});
-    const [playerState, setPlayerState] = useState(1) // 0 closed , 1 small , 2 full
+    const [playerState, setPlayerState] = useState(2) // 0 closed , 1 small , 2 full
     const [que, setQue] = useState(['https://www.youtube.com/watch?v=Wt0Z3DSMGSk', 'https://www.youtube.com/watch?v=Vv_9kgnoFSM&', 'https://www.youtube.com/watch?v=Fc-5d7V1Rjg'])
     const [currentTime, setCurrentTime] = useState(0)
+    const [activeFrame, setActiveFrame] = useState(0)
+
+
     useEffect(() => {
         $.getJSON('https://noembed.com/embed',
             { format: 'json', url: que[index] }, function (data) {
@@ -33,9 +38,9 @@ function MusicPlayer() {
                 console.log(vidData)
             })
     }, [index]);
+
     const [vidData, setVidData] = useState()
-
-
+    const swiperRef = useRef();
     return (
         <>
             <ReactPlayer
@@ -53,108 +58,86 @@ function MusicPlayer() {
                     setProgressObj(o);
                     setCurrentTime(playerRef.current.getCurrentTime());
                 }}
+
                 onEnded={() => {
                     if (que.length == 0) {
                         return null;
                     }
                     if (index == que.length - 1) {
                         setIndex(0)
+                        swiperRef.current.slideTo(0)
                         return null;
                     }
+
                     setIndex(index + 1)
+                    swiperRef.current.slideTo(index + 1)
                 }} />
 
             {playerState == 2 ?
                 <div className="mega-music-cnt">
                     <div className="mega-music-thumbnail" style={{ 'backgroundImage': `url('http://img.youtube.com/vi/${que[index].split("v=")[1].split("&")[0]}/maxresdefault.jpg')` }}>
-                        <div className="mega-music-info-cnt-2"></div>
-                        <div className="mega-music-info-cnt"></div>
                         
-                        <div className="mega-music-icon">
-                            <SiApplemusic />
-                        </div>
+                        <div className="mega-music-info-cnt"></div>
+                        <div className="mega-music-info-cnt-2"></div>
 
+                        
 
                         {/* <div className='mega-music-title' style={{backgroundColor:''}}>{vidData === undefined ? 'undefined' : <p>{vidData.title}</p> }</div> */}
-                        <div className="mega-music-menu-btm-border">
-                            <Slider
-                                className='mega-music-slider'
-                                max={Math.ceil(duration)}
-                                defaultValue={0}
-                                value={Math.ceil(progessObj.playedSeconds)}
-                                onChange={(v) => {
-                                    playerRef.current.seekTo(parseFloat(v), "seconds");
-                                }}
-                                style={{ width: '100%' , margin:'auto 0' ,position:'auto', left:'0%',top:'-20%'}}
-                                step={0.1}
-                                trackStyle= {{backgroundColor:'#1C1C1C ' , color:' #1C1C1C !important', height:'0.75em'}}
-                                railStyle=  {{backgroundColor:'gray' , color:' #1C1C1C !important' , height:'0.75em'}}
-                                handleStyle={[{width:'0.9em',height:'0.9em',top:'60%',border:'2px solid black',boxShadow:'0 0 5px gray'}]}
-                            />
-                        </div>
-                        
-                        <div className="mega-music-buttom-container">
-                            <div className="mega-navigation-l"
-                                onClick={() => {
-                                if (que.length == 0) {
-                                    return null;
-                                }
-                                if (index == 0) {
-                                    setIndex(que.length - 1)
-                                    return null;
-                                }
-                                setIndex(index - 1)}}
-                            >
-                                <AiFillBackward />
-                            </div>
-                            <div className="mega-play" onClick={() => setPlaying(!isPlaying)}>
-                                {isPlaying ? <GrPauseFill className='mega-play-btn mega-pause' stroke='#1C1C1C' /> : <GrPlayFill className='mega-play-btn mega-playing' stroke='#1C1C1C' />}
-                            </div>
-                            <div className="mega-navigation-r"
-                                onClick={() => {
-                                    if (que.length == 0) {
-                                        return null;
-                                    }
-                                    if (index == que.length - 1) {
-                                        setIndex(0)
-                                        return null;
-                                    }
-                                    setIndex(index + 1)
-                                }}
 
+                        {/* <div className="mega-music-main-btn-cnt">
+                            <div className="mega-play" onClick={() => setPlaying(!isPlaying)}>
+                                {isPlaying ? <GrPauseFill className='mega-play-btn mega-pause' color='white' /> : <GrPlayFill className='mega-play-btn mega-playing' stroke='#1C1C1C' />}
+                            </div>
+                        </div> */}
+                        <div className="mega-player-main-body">
+                            <Swiper
+                                spaceBetween={75}
+                                slidesPerView={1}
+                                onSlideChange={(e) => (setIndex(e.activeIndex))}
+                                style={{ zIndex: 10, position: 'absolute' }}
+                                modules={[EffectFade]}
+                                onSwiper={(swiper) => {
+                                    swiperRef.current = swiper;
+                                }}
+                                effect={'fade'}
                             >
-                                <AiFillForward />
+                                {que.map((url, index) => {
+
+                                    return (
+                                        <SwiperSlide key={index} 
+                                        
+                                        
+                                        style={{
+                                            'backgroundImage': `url('http://img.youtube.com/vi/${url.split("v=")[1].split("&")[0]}/maxresdefault.jpg')`,
+                                            backgroundPosition: 'center',
+                                            backgroundSize: '230%',
+                                            outline: '0.8   em solid #cadffa',
+                                            boxShadow: 'inset #ffffff73 -6px -6px 14px,inset rgba(94 , 104 ,140 ,.288) 7px 7px 14px , inset #cadffa 0 0 0 10px ',
+                                            overflow: 'hidden',
+                                            border:'0.5px solid rgba(255,255,255,0.2);'
+                                        }}>
+                                        </SwiperSlide>)
+                                })}
+                            </Swiper>
+                            <div className="mega-music-menu-btm-border">
+                                <Slider
+                                    className='mega-music-slider'
+                                    max={Math.ceil(duration)}
+                                    defaultValue={0}
+                                    value={Math.ceil(progessObj.playedSeconds)}
+                                    onChange={(v) => {
+                                        playerRef.current.seekTo(parseFloat(v), "seconds");
+                                    }}
+                                    style={{ width: '90%', margin: 'auto 0', marginTop: '0.75em', position: 'auto', left: '1.5em' , top: '-20%' }}
+                                    step={0.1}
+                                    trackStyle={{ backgroundColor: ' rgba(94 , 104 ,140 ,.488)', color: ' #1C1C1C !important', height: '0.75em' }}
+                                    railStyle={{ backgroundColor: '#ffffff50', color: ' #1C1C1C !important', height: '0.75em' }}
+                                    handleStyle={[{ width: '0.9em', height: '0.9em', top: '60%', border: '0px solid black', boxShadow: '0 0 5px gray' }]}
+                                />
+                                
                             </div>
                         </div>
-                        <Swiper
-                            spaceBetween={550}
-                            slidesPerView={1}
-                            onSlideChange={() => console.log('slide change')}
-                            onSwiper={(swiper) => console.log(swiper)}
-                            style={{zIndex:10 , position:'absolute'}}
-                            preloadImages={true}
-                        >
-                            {que.map((url , index)=>{
-                                return(
-                                <SwiperSlide key={index} style={{
-                                    'backgroundImage': `url('http://img.youtube.com/vi/${url.split("v=")[1].split("&")[0]}/maxresdefault.jpg')`,
-                                    backgroundPosition: 'center',
-                                    backgroundSize:'230%',
-                                    outline: '0.3em solid rgba(255, 255, 255, 0.6)',
-                                    boxShadow: '26px 24px 15px -3px rgba(0,0,0,0.3)',
-                                    overflow:'hidden'}}>
-                                    <div style={{
-                                        position:'absolute',
-                                        width:'10em',
-                                        height:'10em',
-                                        backgroundColor:'white',
-                                        bottom:'calc(0% - 5em)',
-                                        right:'-5em',
-                                        borderRadius:'50%'
-                                    }}></div>
-                                </SwiperSlide>)
-                            })}
-                        </Swiper>
+
                     </div>
 
                 </div>
@@ -175,8 +158,8 @@ function MusicPlayer() {
                                     playerRef.current.seekTo(parseFloat(v), "seconds");
                                 }}
                                 style={{ width: '60%' }}
-                                trackStyle={{backgroundColor:'#ff4f7b85' , color:' #ff4f7b85 !important'}}
-                                railStyle={{backgroundColor:'##57bef585 !important' , color:'#57bef585 !important'}}
+                                trackStyle={{ backgroundColor: '#ff4f7b85', color: ' #ff4f7b85 !important' }}
+                                railStyle={{ backgroundColor: '##57bef585 !important', color: '#57bef585 !important' }}
 
                             />
                             <span className='mini-music-currentTime'>
@@ -240,3 +223,4 @@ function MusicPlayer() {
 }
 
 export default MusicPlayer
+
